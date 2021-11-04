359b5c0b1c5a8b1599901a4f02219c80a5c6e7f1
phamanhthien1 <62327329+phamanhthien1@users.noreply.github.com>
2021-11-04 18:37:18 +0700

    upload project

---
+++ admin/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+$config['base_url'] = "http://localhost/LT_WEB/admin/";
+
+
+$config['default_module'] = 'user';
+$config['default_controller'] = 'index';
+$config['default_action'] = 'update_info';
+
+
+
+
+
+
+
+
